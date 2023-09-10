6c529c8f9e3c175998622b6d4c32ddb4474672b4
Arpit <arpitvidhale08@gmail.com>
2023-09-10 19:29:51 +0530

    packages done

---
+++ packages/inner/demo.java
@@ -0,0 +1,10 @@
+package packages.inner;
+
+public class demo {
+    public void display(){
+        System.out.println(" msg from inner");
+    }
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ packages/outer/demo2.java
@@ -0,0 +1,11 @@
+package packages.outer;
+ 
+import packages.inner.demo;
+public class demo2 {
+    
+    public static void main(String[] args) {
+         demo d = new demo();
+         d.display();
+         
+    }
+}
